e89279ec5b632cf88207bd404b963f6abcb07d65
Japoivvb <jose.portugal@stucom.com>
2024-09-26 18:24:20 +0200

    formats code

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,19 +6,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //configured to listen for and handle HTTP requests
+@RestController // configured to listen for and handle HTTP requests
 @RequestMapping("/cashcards") // from url starting with
 class CashCardController {
-	
-	@GetMapping("/{requestedId}") // GET requests that match cashcards/{requestedID} will be handled by this method.
+
+	@GetMapping("/{requestedId}") // GET requests that match cashcards/{requestedID} will be handled by this
+									// method.
 //	private ResponseEntity<String> findById() {
 	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
 //	      return ResponseEntity.ok("{}");
 		if (requestedId.equals(99L)) {
-	        CashCard cashCard = new CashCard(99L, 123.45);
-	        return ResponseEntity.ok(cashCard);
-	    } else {
-	        return ResponseEntity.notFound().build();
-	    }
-	   }
+			CashCard cashCard = new CashCard(99L, 123.45);
+			return ResponseEntity.ok(cashCard);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
